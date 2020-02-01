acfc3b2103ffa2ed03898a5f649b4d36f70c4b35
drechsler <drechsler@tu-berlin.de>
2020-02-01 01:19:36 +0100

    Added Bitfield

--- rtl.js
+++ rtl.js
@@ -1,5 +1,5 @@
 
-var mux = require("./shapes/mux.js")
+var bitfield = require("./shapes/bitfield.js")
 var entity = require("./shapes/entity.js")
 var bus = require("./shapes/bus.js")
 
@@ -7,14 +7,20 @@ var bus = require("./shapes/bus.js")
 Draw.loadPlugin(function(ui){window.ui=ui;});
 
 
-Draw.loadPlugin(function(ui) {
-  // Adds custom sidebar entry
-  ui.sidebar.addRTLPalette();
-});
 
 Sidebar.prototype.addRTLPalette = function() {
+  var BitfieldTxt = `[
+{ "name": "D", "bits": 1, "attr": "", "type":6 },
+{ "name": "L", "bits": 1, "attr": "", "type":5 },
+{ "name": "E", "bits": 1, "attr": "", "type":4 },
+{ "name": "I", "bits": 1, "attr": "", "type":3 },
+{ "name": "F", "bits": 1, "attr": "", "type":2 },
+{ "name": "T", "bits": 1, "attr": "", "type":1 },
+{ "name": "I", "bits": 1, "attr": "", "type":0 },
+{ "name": "B", "bits": 1, "attr": "" }
+]`;
   this.addPaletteFunctions('rtl','RTL', true, [
-    this.createVertexTemplateEntry('shadow=0;dashed=0;align=center;html=1;strokeWidth=1;shape=mxgraph.rtl.abstract.mux;',  80, 120, 'Mux', 'Mux', null, null, this.getTagsForStencil('mxgraph.rtl.abstract', 'mux', 'rtl ').join(' ')),
+    this.createVertexTemplateEntry('shadow=0;dashed=0;align=left;strokeWidth=1;shape=mxgraph.rtl.abstract.bitfield;labelBackgroundColor=#ffffff;noLabel=1;',  200, 100, BitfieldTxt, 'Bitfield', null, null, this.getTagsForStencil('mxgraph.rtl.abstract', 'bitfield', 'rtl ').join(' ')),
     this.createVertexTemplateEntry('shadow=0;dashed=0;align=center;html=1;strokeWidth=1;shape=mxgraph.rtl.abstract.entity;container=1;collapsible=0;',  80, 120, 'Entity', 'Entity', null, null, this.getTagsForStencil('mxgraph.rtl.abstract', 'entity', 'rtl ').join(' ')),
     this.createVertexTemplateEntry('shadow=0;dashed=0;align=center;html=1;strokeWidth=1;shape=mxgraph.rtl.abstract.entity;left=,:clk;right=,:np;top=0;bottom=0;drawPins=0;snapToPoint=1;resizable=0;editable=1;',  40, 60, '', 'Register', null, null, this.getTagsForStencil('mxgraph.rtl.abstract', 'reg', 'rtl ').join(' ')),
     this.createVertexTemplateEntry('shadow=0;dashed=0;align=center;html=1;strokeWidth=1;shape=mxgraph.rtl.abstract.entity;verticalAlign=top;spacing=0;spacingTop=7;left=1;right=:n;top=0;bottom=0;drawPins=1;snapToPoint=1;resizable=0;editable=0;',  60, 60, '1', 'NOT', null, null, this.getTagsForStencil('mxgraph.rtl.abstract', 'not', 'rtl ').join(' ')),
@@ -28,3 +34,7 @@ Sidebar.prototype.addRTLPalette = function() {
   ]);
 }
   
+Draw.loadPlugin(function(ui) {
+  // Adds custom sidebar entry
+  ui.sidebar.addRTLPalette();
+});
---
+++ shapes/bitfield.js
@@ -0,0 +1,71 @@
+var render = require('bit-field/lib/render');
+var onml = require('onml');
+/**
+* Extends mxShape.
+*/
+function mxShapeRTLBitfield(bounds, fill, stroke, strokewidth) {
+	mxShape.call(this);
+	this.bounds = bounds;
+	this.fill = fill;
+	this.stroke = stroke;
+	this.strokewidth = (strokewidth != null) ? strokewidth : 1;
+};
+/**
+* Extends mxShape.
+*/
+mxUtils.extend(mxShapeRTLBitfield, mxShape);
+
+mxShapeRTLBitfield.prototype.cst = {
+	SHAPE_BITFIELD: 'mxgraph.rtl.abstract.bitfield'
+};
+
+mxShapeRTLBitfield.prototype.customProperties = [
+	{ name: 'vspace', dispName: 'Vertical Space', type: 'int', min: 1, max: 2000, defVal: 80 },
+	{ name: 'hspace', dispName: 'Horizontal Space', type: 'int', min: 1, max: 2000, defVal: 320 },
+	{ name: 'lanes', dispName: 'Number of Lanes', type: 'int', min:1, max:1024, defVal: 1 },
+	{ name: 'bits', dispName: 'Number of Fields', type: 'int', min:1, max:1024, defVal: 8 },
+	{ name: 'compact', dispName: 'Compact', type: 'boolean', defVal: false },
+	{ name: 'bigendian', dispName: 'Big Endian', type: 'boolean', defVal: false },
+];
+
+/**
+* Function: paintVertexShape
+* Untitled Diagram.drawio
+* Paints the vertex shape.
+*/
+mxShapeRTLBitfield.prototype.paintVertexShape = function (c, x, y, w, h) {
+	var vspace = parseInt(mxUtils.getValue(this.style, 'vspace', '80'));
+	var hspace = parseInt(mxUtils.getValue(this.style, 'hspace', '320'));
+	var lanes  = parseInt(mxUtils.getValue(this.style, 'lanes', '1'));
+	var bits   = parseInt(mxUtils.getValue(this.style, 'bits', '8'));
+	var compact = mxUtils.getValue(this.style, 'compact', false);
+	var bigendian = mxUtils.getValue(this.style, 'bigendian', false);
+
+	var options = {
+		vspace:vspace,
+		hspace:hspace,
+		lanes:lanes,
+		bits:bits,
+		compact:compact,
+		bigendian:bigendian,
+		fontsize:this.style.fontSize,
+		fontfamily:this.style.fontFamily,
+	}
+
+	var jsonml = []
+
+	jsonml[0] = "g";
+	jsonml[1] = {};
+	var tmp = render(JSON.parse(this.state.cell.value),options);
+	var scale = Math.min(w/tmp[1].width,h/tmp[1].height);
+	jsonml[1].transform="translate("+x+","+y+") scale("+scale+","+scale+")";
+	jsonml[2] = tmp;
+	this.node.innerHTML = onml.stringify(jsonml);
+}
+
+mxCellRenderer.registerShape(mxShapeRTLBitfield.prototype.cst.SHAPE_BITFIELD, mxShapeRTLBitfield);
+
+mxShapeRTLBitfield.prototype.getConstraints = function (style, w, h) {
+	var constr = [];
+	return constr;
+}
